4250bfdb8de92e4118c1765f3ae6f0652d9f4feb
WKolaj <WKolaj@gmail.com>
2020-07-03 10:59:01 +0200

    Added language change after login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,55 +2,65 @@ import { LOGIN_USER, LOGIN_USER_WITH_JWT, LOGOUT_USER } from "./types";
 import { logout, login, loginWithJWT } from "../services/authService";
 import { wrapAsyncActionToHandleError } from "./asyncActionsErrorWrapper";
 import { fetchCurrentUserDataActionCreator } from "./data";
+import { changeLanguageActionCreator } from "./language";
+import { exists } from "../utilities/utilities";
 
-export const loginUserWithJWTActionCreator = function(jwt) {
-  return async function(dispatch, getState) {
+export const loginUserWithJWTActionCreator = function (jwt) {
+  return async function (dispatch, getState) {
     let currentUser = loginWithJWT(jwt);
 
     await dispatch({
       type: LOGIN_USER_WITH_JWT,
       payload: {
-        user: currentUser
-      }
+        user: currentUser,
+      },
     });
 
     //Refreshing data also regarind current user data
     await dispatch(fetchCurrentUserDataActionCreator());
+
+    //Changing language to default for given user
+    if (exists(currentUser.defaultLang))
+      await dispatch(changeLanguageActionCreator(currentUser.defaultLang));
   };
 };
 
-export const loginUserWithJWTActionCreatorWrapped = function(jwt) {
+export const loginUserWithJWTActionCreatorWrapped = function (jwt) {
   return wrapAsyncActionToHandleError(loginUserWithJWTActionCreator(jwt));
 };
 
-export const loginUserActionCreator = function(email, password) {
-  return async function(dispatch, getState) {
+export const loginUserActionCreator = function (email, password) {
+  return async function (dispatch, getState) {
     let currentUser = await login(email, password);
     await dispatch({
       type: LOGIN_USER,
       payload: {
-        user: currentUser
-      }
+        user: currentUser,
+      },
     });
 
     //Refreshing data also regarind current user data
     await dispatch(fetchCurrentUserDataActionCreator());
+
+    //Changing language to default for given user
+    if (exists(currentUser.defaultLang))
+      await dispatch(changeLanguageActionCreator(currentUser.defaultLang));
   };
 };
 
-export const loginUserActionCreatorWrapped = function(email, password) {
+export const loginUserActionCreatorWrapped = function (email, password) {
   return wrapAsyncActionToHandleError(
     loginUserActionCreator(email, password),
     "Niepoprawny email lub hasło"
   );
 };
 
-export const logoutUserActionCreator = function() {
-  return async function(dispatch, getState) {
+export const logoutUserActionCreator = function () {
+  return async function (dispatch, getState) {
     logout();
 
     await dispatch({
-      type: LOGOUT_USER
+      type: LOGOUT_USER,
     });
 
     //Refreshing data also regarind current user data
@@ -58,6 +68,6 @@ export const logoutUserActionCreator = function() {
   };
 };
 
-export const logoutUserActionCreatorWrapped = function() {
+export const logoutUserActionCreatorWrapped = function () {
   return wrapAsyncActionToHandleError(logoutUserActionCreator());
 };
